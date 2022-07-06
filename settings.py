19aea2225e93966cf3f099257da5afd1c8d15295
vladyslavgorban <vladislav.gorban@gmail.com>
2022-07-06 12:30:11 +0300

    ship can move right (+1)

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings():
     def __init__(self):
         """initializy game settings"""
         # screen parameters
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1024
+        self.screen_height = 600
         self.bg_color = (0,181,226)
