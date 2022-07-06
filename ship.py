8c9a30f311b71c05768afbe94e3ee7949f44d74a
vladyslavgorban <vladislav.gorban@gmail.com>
2022-07-06 11:34:35 +0300

    draft of ship class + ship image

---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ship():
+    """ship movement class"""
+
+    def __init__(self, ai_game):
+        """initiates ship and sets start position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # load ship picture, gat rectangle
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        # each new ship appears bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """draw a ship in current position"""
+        self.screen.blit(self.image, self.rect)
