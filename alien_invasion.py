b67f7f60f8771a36926aa4559e6aa7bb065fc7ce
vladyslavgorban <vladislav.gorban@gmail.com>
2022-07-06 11:16:04 +0300

    Start project. With settings file

---
+++ alien_invasion.py
@@ -0,0 +1,39 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+class AlienInsavion:
+    """manage resources and game behavior class"""
+
+    def __init__(self):
+        """initialize game and create resources"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        # det default background color
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """rum main game route"""
+        while True:
+            # track mouse and keyboard behaviour
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # renew screen in every loop
+            self.screen.fill(self.bg_color)
+
+            # display last screen
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # create an instance and run a game
+    ai = AlienInsavion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """class for all settings in Alien Invasion"""
+
+    def __init__(self):
+        """initializy game settings"""
+        # screen parameters
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color - (230, 230, 230)
