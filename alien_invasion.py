73c524d38e8129a06f7a4bc58cfcfe798d5fc3ca
vladyslavgorban <vladislav.gorban@gmail.com>
2022-07-06 17:43:05 +0300

    refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,13 +27,7 @@ class AlienInsavion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            
-            # delete bullets after they gone screen
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -69,6 +63,16 @@ class AlienInsavion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """update bullets postition and delete old bullets"""
+        # bullets position update
+        self.bullets.update()
+            
+        # delete bullets after they gone screen
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """update screen"""
