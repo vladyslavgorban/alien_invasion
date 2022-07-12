e83c285d5a980b874c177f4b16670c4fd4e54fc1
vladyslavgorban <vladislav.gorban@gmail.com>
2022-07-12 18:29:01 +0300

    crate all alien fleet (several lines)

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,20 +83,29 @@ class AlienInsavion:
         # create an alien and calculate number of aliens in line
         # space between aliens equals aline's width
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # create first line of aliens
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        """define numbers of rows with current screen size"""
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - 
+                                (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number):
-        # create alien and place it in the line
+        # create alien fleet
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+
+    def _create_alien(self, alien_number, row_number):
+        # create alien and place it in the row
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
