c7e68e97bc0f76bfa4ce56b6aafef200a1999460
vladyslavgorban <vladislav.gorban@gmail.com>
2022-07-15 19:33:46 +0300

    scoring: rounded score to 10 on display

--- scoreboard.py
+++ scoreboard.py
@@ -18,7 +18,8 @@ class Scoreboard():
 
     def prep_score(self):
         """transform text scrore into image"""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, 
                     self.text_color, self.settings.bg_color)
 
